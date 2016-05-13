cba97727b386fba37bb2e6ff2a9a456dc2e27f46
nagabhushanamn <nagabhushanamn@gmail.com>
2016-05-13 13:50:17 +0530

    initial commit

---
+++ NG/shop_v1/js/app.js
@@ -0,0 +1,77 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+(function() {
+
+	var storeModule = angular.module('store', []);
+
+	// ----------------------------------------------
+	// Model ( data ) ( shud loaded from server/local-storage )
+	var items = [ {
+		name : 'Laptop',
+		price : 198000, // number
+		desc : 'New Model',
+		canBuy : true,
+		notAvailable : false,
+		make : Date.now(),
+		discount : 1000
+	}, {
+		name : 'Mobile',
+		price : 9000,
+		desc : 'New Model',
+		canBuy : true,
+		notAvailable : false,
+		make : Date.now(),
+		discount : 500
+	} ];
+	// ----------------------------------------------
+
+	// Using DOM ( plain JS )
+
+	// document.querySelector('h4').innerHTML = item.name;
+	// document.querySelector('h5').innerHTML = item.price;
+	// document.querySelector('p').innerHTML = item.desc;
+
+	// ----------------------------------------------
+
+	// using jQuery
+
+	// $(function(){
+	// $('h4').text(item.name);
+	// $('h5').text(item.price);
+	// $('p').text(item.desc);
+	// })
+
+	// ----------------------------------------------
+
+	// Controller
+	storeModule.controller('StoreController', function($scope, $filter) {
+		// this.product=item;
+		// $scope.product = item; // ViewModel
+		$scope.products = items; // ViewModel
+
+		console.log($filter('uppercase')(items[0].desc));
+		console.log($filter('priceDiscount')(1000, 100));
+
+	});
+
+	// ----------------------------------------------
+
+	storeModule.filter('priceDiscount', function($filter) {
+		return function(originalPrice, discount) {
+			if (angular.isNumber(originalPrice)) {
+				if (!discount) {
+					return originalPrice - 1;
+				} else {
+					return originalPrice - discount;
+				}
+			} else {
+				return originalPrice;
+			}
+		};
+	});
+
+	// ----------------------------------------------
+
+})();
